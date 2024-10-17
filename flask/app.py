05c73f08d3cab5861edb8aecb5c5bb330f6e40c6
milan <milan.nicolas@rocketmail.com>
2024-10-17 07:31:15 +0200

    start app for backend, actualy just to try the front

---
+++ flask/app.py
@@ -0,0 +1,27 @@
+import mlflow
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/api/v1/predict', methods=['POST'])
+def predict():
+    # data = request.get_json()
+    # model_uri = "models:/mon_modele/Production"
+    # model = mlflow.pyfunc.load_model(model_uri)
+
+    # prediction = model.predict([data['input']])
+    prediction = ["python", "machine-learning", "data-science"]
+    
+    return jsonify({'tags': prediction})
+
+@app.route('/api/v1/validate', methods=['POST'])
+def validate():
+    data = request.get_json()
+    tags = data.get('tags')
+
+    print(f'Tags validés: {tags}')
+
+    return jsonify({'status': 'success', 'tags': tags})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
