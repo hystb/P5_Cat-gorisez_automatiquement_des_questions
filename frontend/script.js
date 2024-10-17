572b7a8a78111d077566b0c8bf95d2d477c3f850
milan <milan.nicolas@rocketmail.com>
2024-10-17 07:32:07 +0200

    full front layout, last added feature input area change with input size

---
+++ frontend/script.js
@@ -0,0 +1,67 @@
+document.getElementById("question").addEventListener("input", function() {
+	console.log(this.scrollHeight);
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight - 40) + "px";
+});
+
+function getTagSuggestions() {
+    const question = document.getElementById('question').value;
+    fetch('http://localhost/flask/api/v1/predict', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ question: question })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const tagSuggestions = document.getElementById('tag-suggestions');
+        tagSuggestions.innerHTML = '';
+
+        data.tags.forEach(tag => {
+            const tagElement = document.createElement('div');
+            tagElement.classList.add('tag');
+            tagElement.textContent = tag;
+            tagElement.onclick = () => selectTag(tagElement);
+            tagSuggestions.appendChild(tagElement);
+        });
+    })
+    .catch(error => console.error('Erreur:', error));
+}
+
+function selectTag(tagElement) {
+    const selectedTagsContainer = document.getElementById('selected-tags');
+    const tagClone = tagElement.cloneNode(true);
+    tagClone.classList.add('selected');
+    selectedTagsContainer.appendChild(tagClone);
+
+    tagElement.style.display = 'none';
+}
+
+function validateTags() {
+    const selectedTags = [];
+    document.querySelectorAll('#selected-tags .tag').forEach(tag => {
+        selectedTags.push(tag.textContent);
+    });
+
+    fetch('http://localhost/flask/api/v1/validate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ tags: selectedTags })
+    })
+    .then(response => response.json())
+    .then(data => {
+		console.log(data);
+		const selectedTagsContainer = document.getElementById('selected-tags');
+        selectedTagsContainer.innerHTML = '';
+		const tagSuggestions = document.getElementById('tag-suggestions');
+		tagSuggestions.innerHTML = '';
+		return data;
+    })
+	.then(data => {
+		alert('Tags validés avec succès ' + data.tags);
+	})
+    .catch(error => console.error('Erreur:', error));
+}
